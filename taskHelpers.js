c2ed8179278162ac231b33926a0a7cf3e220e76f
galica9999 <galica9999@gmail.com>
2021-11-16 19:04:52 -0500

    renamed files, changed some task code
    updated the task code to be a grid format

---
+++ taskHelpers.js
@@ -0,0 +1,6 @@
+const edit = document.getElementById('edit');
+password.addEventListener('click', (event) => {
+  event.preventDefault();
+  $('#edit-modal');
+  $('#edit-modal').modal('show');
+});
